30599e3cab8b54d3ba60ec55dcb8ace8d7f1f70b
Dave Ackerman <dmackerman@gmail.com>
2012-09-13 16:11:50 -0400

    login styles

--- app/view/Login.js
+++ app/view/Login.js
@@ -4,28 +4,29 @@ Ext.define('D3Mobile.view.Login', {
     requires   : ['Ext.form.Panel', 'Ext.field.Text', 'Ext.TitleBar', 'Ext.form.FieldSet', 'Ext.Button'],
     config     : {
         fullscreen : true,
+        cls: 'login',
+        padding: 10,
         items      : [
-            {
-                xtype            : 'component',
-                docked           : 'top',
-                html             : "<h1>Diablo 3 Mobile Companion</h1>",
-                styleHtmlContent : true
-            },
             {
                 xtype            : 'component',
                 styleHtmlContent : true,
                 html             : ''.concat(
-                    '<div class="login">',
-                        '<div class="battle-tag-label">Please enter your BattleTag</div>',
+                    '<div class="login-container">',
+                        '<h1 class="d3-color-orange">Mobile Companion</h1>',
+                        '<div class="d3-logo"></div>',
                         '<div class="battle-tag-input">',
-                                '<input type="text" name="battleTag" placeholder="Battle Tag"/>',
-                                '<input type="number" name="battleTagNum" placeholder="####"/>',
+                            '<form>',
+                                '<input type="text" name="battleTag" placeholder="Battle Tag" required />',
+                                '<input type="text" name="battleTagNum" placeholder="####" required />',
+                            '</form>',
                         '</div>',
                     '</div>'
                 )
             },
             {
                 xtype : 'button',
+                cls: 'loginBtn',
+                ui: 'loginButton',
                 text  : 'Log In'
             }
         ]
