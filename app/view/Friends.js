cdaa97b095710e13abae359a5a4cb5ccb42a5c0e
Dave Ackerman <dmackerman@gmail.com>
2012-09-13 22:30:57 -0400

    nav

--- app/view/Friends.js
+++ app/view/Friends.js
@@ -4,6 +4,7 @@ Ext.define('D3Mobile.view.Friends', {
     requires : ['Ext.TitleBar'],
     config   : {
         title : 'Friends',
+        cls   : 'friends',
         items : [
             {
                 xtype    : 'titlebar',
